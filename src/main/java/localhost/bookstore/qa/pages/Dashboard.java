7f6fb04c6939cdcde7352829ae5a072ed4bd6480
Shubhamsah8 <shubhamsah086@gmail.com>
2024-03-29 11:13:04 +0530

    Automating React Bookstore application

--- src/main/java/localhost/bookstore/qa/pages/Dashboard.java
+++ src/main/java/localhost/bookstore/qa/pages/Dashboard.java
@@ -1,6 +1,49 @@
 package localhost.bookstore.qa.pages;
 
 import localhost.bookstore.qa.baseclass.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Dashboard extends BaseClass {
+
+    @FindBy(xpath = "//div[text()='BookStore']")
+    WebElement bookStoreText;
+
+    @FindBy(xpath = "//img[contains(@role, 'presentation')]")
+    WebElement bookStoreIcon;
+
+    @FindBy(xpath = "//input[contains(@aria-label, 'search')]")
+    WebElement searchBar;
+
+    @FindBy(xpath = "//button[1]")
+    WebElement profile;
+
+    @FindBy(xpath = "//button[2]")
+    WebElement cart;
+
+    @FindBy(xpath = "//li[text()='Log Out']")
+    WebElement logout;
+
+    public Dashboard(){
+        PageFactory.initElements(driver, this);
+    }
+
+    // Method to search the item in the Dashboard
+    public void SearchBar(String searchItem){
+        searchBar.sendKeys(searchItem);
+    }
+
+    // Method to log out from the dashboard
+    public LoginPage Logout(){
+        profile.click();
+        logout.click();
+
+        return new LoginPage();
+    }
+
+    // Method to click and check the functionality of the Cart option
+    public void Cart(){
+        cart.click();
+    }
 }
---
+++ src/test/java/localhost/bookstore/qa/testcases/DashboardTest.java
@@ -0,0 +1,80 @@
+package localhost.bookstore.qa.testcases;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import localhost.bookstore.qa.baseclass.BaseClass;
+import localhost.bookstore.qa.pages.Dashboard;
+import localhost.bookstore.qa.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DashboardTest extends BaseClass {
+
+    Dashboard dashboard;
+    LoginPage loginPage;
+
+    public DashboardTest(){
+        super();
+    }
+
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        try{
+            initialization();
+            loginPage = new LoginPage();
+            dashboard = loginPage.Login(properties.getProperty("email"), properties.getProperty("password"));
+        }
+        catch (WebDriverException e){
+            e.printStackTrace();
+        }
+    }
+
+    // Method to test the search bar
+    @Test(priority = 1)
+    @Description("Verify if the Search bar is properly working or not")
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Searching Items")
+    public void searchBarTest() throws InterruptedException{
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[contains(@aria-label, 'search')]")));
+        dashboard.SearchBar(properties.getProperty("searchItem"));
+    }
+
+    // Method to test Log out feature
+    @Test(priority = 2)
+    @Description("Verify if the user is properly is logging out or not")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Logging out")
+    public void logOutTest() throws InterruptedException{
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[1]")));
+        dashboard.Logout();
+    }
+
+    // Method to test the Cart option feature
+    @Test(priority = 3)
+    @Description("Verify if the user navigate to the Cart page or not")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Go to the Cart")
+    public void cartTest() throws InterruptedException{
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[2]")));
+        dashboard.Cart();
+    }
+
+    @AfterMethod
+    public void tearUp(){
+        //driver.quit();
+    }
+}
